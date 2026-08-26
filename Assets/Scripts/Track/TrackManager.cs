1d6eb0bf1194814655867b620f0171d911e11773
Hasan Cetin <hasan@example.com>
2026-08-26 20:10:26 +0300

    M1: Fix missing using statements - Add TurboGaraj.Vehicle to TrackManager.cs and TurboGaraj.Economy to GarageUI.cs

--- Assets/Scripts/Track/TrackManager.cs
+++ Assets/Scripts/Track/TrackManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TurboGaraj.Vehicle;
 
 namespace TurboGaraj.Track
 {
--- Assets/Scripts/UI/GarageUI.cs
+++ Assets/Scripts/UI/GarageUI.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TurboGaraj.Economy;
 
 namespace TurboGaraj.UI
 {
